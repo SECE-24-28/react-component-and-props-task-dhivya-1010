582b54e7e2f574d12cd7be1e498f0904f54dd763
dhivya-1010 <dhivya.v2024cse@sece.ac.in>
2026-06-08 12:40:35 +0530

    react app created

--- js.js
+++ js.js
@@ -1,101 +1,102 @@
-// let a = 10;
-// console.log(a);
+//VARIABE DECLARATION
+let a = 10;
+console.log(a);
 
-// const a = 10;
-// const b = 20;
+const a = 10;
+const b = 20;
 
-// const add = (a,b)=>{
-//     console.log(a+b);
-// };
+//ARROW FUNCTION/LAMBDA FUNCTION
+const add = (a,b)=>{
+    console.log(a+b);
+};
 
-// add(a,b);
+add(a,b);
 
 //TEMPLATE LITERALS
-//let a = "Dhivya";
-//console.log("welcome " + a);
-//console.log(`welcome ${a}`);
+let a = "Dhivya";
+console.log("welcome " + a);
+console.log(`welcome ${a}`);
 
 
-// //ARRAY DESTRUCTURING
-// const arr = ["a","abs","min"]
-// console.log(arr[0]);
-// const [a,b,c] = arr;
-// console.log(a);
-// console.log(b);
+//ARRAY DESTRUCTURING
+const arr = ["a","abs","min"]
+console.log(arr[0]);
+const [a,b,c] = arr;
+console.log(a);
+console.log(b);
 
 //OBJECT DESTRUCTURING
-// const stud = {
-//     name: "dhivya",
-//     age:21
-// }
+const stud = {
+    name: "dhivya",
+    age:21
+}
 
-// const{name,age} = stud;
-// console.log(name)
-// console.log(age)
+const{name,age} = stud;
+console.log(name)
+console.log(age)
 
-// //node - runtime environment of javascript
-
+// //node - runtime environment of javascript N
 //SPREAD OPERATOR
 //1.ARRAYS
-// const a = [1,2,3]
-// const b = [...a]
-// console.log(b)
+const a = [1,2,3]
+const b = [...a]
+console.log(b)
 
-// const a = [1,2,3]
-// const b = [4,5,6]
-// const c = [...a,...b]
-// console.log(c)
+const a = [1,2,3]
+const b = [4,5,6]
+const c = [...a,...b]
+console.log(c)
 
 
 // //SPREAD OPERATOR
-// //2. OBJECT
-// const stud = {
-//     "name":"Dhivya",
-//     "age":21
-// }
+//2. OBJECT
+const stud = {
+    "name":"Dhivya",
+    "age":21
+}
 
-// const s1 = {
-//     ...stud,
-//     "city":"cbe"
-// }
+const s1 = {
+    ...stud,
+    "city":"cbe"
+}
 
-// console.log(s1)
+console.log(s1)
 
 //REST OPERATOR
 //to collect the values without using loops or sth
-// const add=(...num)=>{
-//     console.log(num)
-// }
-// add(1,2,3,4,5,7)
+const add=(...num)=>{
+    console.log(num)
+}
+add(1,2,3,4,5,7)
 
 //MAP
-// const a = [1,2,3,4,5]
+const a = [1,2,3,4,5]
 
-// //map
-// const r=a.map(n=>n*2)
-// console.log(r)
+//map
+const r=a.map(n=>n*2)
+console.log(r)
 
 
-// //normal fn
-// function add(n){
-//     return n*2;
-// };
+//normal fn
+function add(n){
+    return n*2;
+};
 
 //FILTER
-// const a = [1,2,3,4,5]
-// const b = a.filter((n)=>n%2==0)
-// console.log(b)
+const a = [1,2,3,4,5]
+const b = a.filter((n)=>n%2==0)
+console.log(b)
 
 //FOR EACH
-// const a = [1,2,3,4]
-// a.forEach((n)=>{
-//     console.log(n)
-// })
+const a = [1,2,3,4]
+a.forEach((n)=>{
+    console.log(n)
+})
 
 //FIND
-// const a = [1,2,3,4]
-// const b = a.find((n)=> n%2==0)
-// console.log(b)
+const a = [1,2,3,4]
+const b = a.find((n)=> n%2==0)
+console.log(b)
 
 //TERNARY OPERATOR
 let a = 19
